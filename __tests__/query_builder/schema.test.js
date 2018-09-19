2ba30776ed452e463d414816d0c98bebda5b5a7d
Daniel Fernando Lourusso <dflourusso@gmail.com>
2018-09-19 17:08:25 -0300

    add qb schema test

---
+++ __tests__/query_builder/schema.test.js
@@ -0,0 +1,37 @@
+import {
+  _createTableColumns,
+  createTable,
+  dropTable
+} from '../../src/query_builder/schema'
+import { types } from '../../src/DataTypes'
+
+const columnMapping = {
+  id: { type: types.INTEGER, primary_key: true },
+  numero: { type: types.INTEGER },
+  codigo_verificacao: { type: types.TEXT },
+  created_at: { type: types.DATETIME },
+  checklist: { type: types.JSON }
+}
+
+describe('create table', () => {
+  const expectedColumns =
+    'id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, numero INTEGER, codigo_verificacao TEXT, created_at DATETIME, checklist TEXT'
+  it('columns string to create a table', () => {
+    const ret = _createTableColumns(columnMapping)
+    expect(ret).toBe(expectedColumns)
+  })
+
+  it('create table statement', () => {
+    const ret = createTable('tests', columnMapping)
+    const expected = `CREATE TABLE IF NOT EXISTS tests (${expectedColumns});`
+    expect(ret).toBe(expected)
+  })
+})
+
+describe('drop table', () => {
+  it('drop table statement', () => {
+    const ret = dropTable('tests')
+    const expected = `DROP TABLE IF EXISTS tests;`
+    expect(ret).toBe(expected)
+  })
+})
