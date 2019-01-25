10d50b5dd7793cbc0c37694faf74aae323ce0abf
Daniel Fernando Lourusso <dflourusso@gmail.com>
2019-01-25 07:32:13 -0200

    better BaseModel test

--- __tests__/BaseModel.test.js
+++ __tests__/BaseModel.test.js
@@ -98,171 +98,131 @@ describe('actions', () => {
     expect(Tmp.repository.dropTable).toHaveBeenCalledTimes(1)
   })
 
-  it('create', done => {
+  it('create', () => {
     const obj = { id: 1, nome: 'Daniel' }
-    Tmp.create(obj)
+    return Tmp.create(obj)
       .then(res => {
         expect(Tmp.repository.insert).toHaveBeenCalledTimes(1)
         expect(Tmp.repository.insert).toBeCalledWith(obj)
         expect(res).toBeInstanceOf(Tmp)
-        done()
       })
-      .catch(done)
   })
 
-  it('update', done => {
+  it('update', () => {
     const obj = { id: 1, nome: 'Daniel' }
-    Tmp.update(obj)
+    return Tmp.update(obj)
       .then(res => {
         expect(Tmp.repository.update).toHaveBeenCalledTimes(1)
         expect(Tmp.repository.update).toBeCalledWith(obj)
         expect(res).toBeInstanceOf(Tmp)
-        done()
       })
-      .catch(done)
   })
 
   describe('save', () => {
-    it('without id', done => {
+    it('without id', () => {
       const tmp = new Tmp({ nome: 'Daniel' })
-      tmp
-        .save()
-        .then(res => {
-          expect(Tmp.repository.insert).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.insert).toBeCalledWith(tmp)
-          expect(Tmp.repository.update).toHaveBeenCalledTimes(0)
-          expect(res).toBeInstanceOf(Tmp)
-          done()
-        })
-        .catch(done)
+      return tmp.save().then(res => {
+        expect(Tmp.repository.insert).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.insert).toBeCalledWith(tmp)
+        expect(Tmp.repository.update).toHaveBeenCalledTimes(0)
+        expect(res).toBeInstanceOf(Tmp)
+      })
     })
 
-    it('with id', done => {
+    it('with id', () => {
       const tmp = new Tmp({ id: 1, nome: 'Daniel' })
-      tmp
-        .save()
-        .then(res => {
-          expect(Tmp.repository.update).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.update).toBeCalledWith(tmp)
-          expect(Tmp.repository.insert).toHaveBeenCalledTimes(0)
-          expect(res).toBeInstanceOf(Tmp)
-          done()
-        })
-        .catch(done)
+      return tmp.save().then(res => {
+        expect(Tmp.repository.update).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.update).toBeCalledWith(tmp)
+        expect(Tmp.repository.insert).toHaveBeenCalledTimes(0)
+        expect(res).toBeInstanceOf(Tmp)
+      })
     })
   })
 
   describe('destroy', () => {
-    it('instance method', done => {
+    it('instance method', () => {
       const tmp = new Tmp({ id: 1, nome: 'Daniel' })
-      tmp
-        .destroy()
-        .then(res => {
-          expect(Tmp.repository.destroy).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.destroy).toBeCalledWith(1)
-          expect(res).toBeTruthy()
-          done()
-        })
-        .catch(done)
+      return tmp.destroy().then(res => {
+        expect(Tmp.repository.destroy).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.destroy).toBeCalledWith(1)
+        expect(res).toBeTruthy()
+      })
     })
 
-    it('static method', done => {
-      Tmp.destroy(1)
-        .then(res => {
-          expect(Tmp.repository.destroy).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.destroy).toBeCalledWith(1)
-          expect(res).toBeTruthy()
-          done()
-        })
-        .catch(done)
+    it('static method', () => {
+      return Tmp.destroy(1).then(res => {
+        expect(Tmp.repository.destroy).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.destroy).toBeCalledWith(1)
+        expect(res).toBeTruthy()
+      })
     })
 
-    it('destroyAll', done => {
-      Tmp.destroyAll()
-        .then(res => {
-          expect(Tmp.repository.destroyAll).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.destroyAll).toBeCalledWith()
-          expect(res).toBeTruthy()
-          done()
-        })
-        .catch(done)
+    it('destroyAll', () => {
+      return Tmp.destroyAll().then(res => {
+        expect(Tmp.repository.destroyAll).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.destroyAll).toBeCalledWith()
+        expect(res).toBeTruthy()
+      })
     })
   })
 
   describe('find', () => {
-    it('found', done => {
-      Tmp.find(1)
-        .then(res => {
-          expect(Tmp.repository.find).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.find).toBeCalledWith(1)
-          expect(res).toBeInstanceOf(Tmp)
-          done()
-        })
-        .catch(done)
+    it('found', () => {
+      return Tmp.find(1).then(res => {
+        expect(Tmp.repository.find).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.find).toBeCalledWith(1)
+        expect(res).toBeInstanceOf(Tmp)
+      })
     })
 
-    it('not found', done => {
-      Tmp.find(999)
-        .then(res => {
-          expect(Tmp.repository.find).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.find).toBeCalledWith(999)
-          expect(res).toBeNull()
-          done()
-        })
-        .catch(done)
+    it('not found', () => {
+      return Tmp.find(999).then(res => {
+        expect(Tmp.repository.find).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.find).toBeCalledWith(999)
+        expect(res).toBeNull()
+      })
     })
   })
 
   describe('findBy', () => {
-    it('found', done => {
+    it('found', () => {
       const where = { numero_eq: 12345, codigo_verificacao_eq: 'AXJFSD' }
-      Tmp.findBy(where)
-        .then(res => {
-          expect(Tmp.repository.findBy).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.findBy).toBeCalledWith(where)
-          expect(res).toBeInstanceOf(Tmp)
-          done()
-        })
-        .catch(done)
+      return Tmp.findBy(where).then(res => {
+        expect(Tmp.repository.findBy).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.findBy).toBeCalledWith(where)
+        expect(res).toBeInstanceOf(Tmp)
+      })
     })
 
-    it('not found', done => {
+    it('not found', () => {
       const fn = jest.fn(() => Promise.resolve(null))
       Tmp.repository.findBy = fn
       const where = { numero_eq: 999 }
-      Tmp.findBy(where)
-        .then(res => {
-          expect(Tmp.repository.findBy).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.findBy).toBeCalledWith(where)
-          expect(res).toBeNull()
-          done()
-        })
-        .catch(done)
+      return Tmp.findBy(where).then(res => {
+        expect(Tmp.repository.findBy).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.findBy).toBeCalledWith(where)
+        expect(res).toBeNull()
+      })
     })
   })
 
   describe('query', () => {
-    it('empty options', done => {
-      Tmp.query({})
-        .then(res => {
-          expect(Tmp.repository.query).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.query).toBeCalledWith({})
-          expect(res).toEqual([])
-          done()
-        })
-        .catch(done)
+    it('empty options', () => {
+      return Tmp.query({}).then(res => {
+        expect(Tmp.repository.query).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.query).toBeCalledWith({})
+        expect(res).toEqual([])
+      })
     })
 
-    it('not empty options', done => {
+    it('not empty options', () => {
       const options = { columns: '*', where: { nome_cont: '%Daniel%' } }
-      Tmp.query(options)
-        .then(res => {
-          expect(Tmp.repository.query).toHaveBeenCalledTimes(1)
-          expect(Tmp.repository.query).toBeCalledWith(options)
-          expect(res).toEqual([])
-          done()
-        })
-        .catch(done)
+      return Tmp.query(options).then(res => {
+        expect(Tmp.repository.query).toHaveBeenCalledTimes(1)
+        expect(Tmp.repository.query).toBeCalledWith(options)
+        expect(res).toEqual([])
+      })
     })
   })
 })
