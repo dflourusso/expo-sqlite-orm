5b95beda1d23a7b00f95c7a79e1ddd567327b9c7
Daniel Fernando Lourusso <dflourusso@gmail.com>
2019-01-25 08:41:55 -0200

    test query builder insertOrReplace

--- __tests__/query_builder/create.test.js
+++ __tests__/query_builder/create.test.js
@@ -5,3 +5,9 @@ it('insert statement', () => {
   const expected = `INSERT INTO tests (id, name) VALUES (?, ?);`
   expect(ret).toBe(expected)
 })
+
+it('insert or replace statement', () => {
+  const ret = qb.insertOrReplace('tests', { id: 1, name: 'Daniel' })
+  const expected = `INSERT OR REPLACE INTO tests (id, name) VALUES (?, ?);`
+  expect(ret).toBe(expected)
+})
